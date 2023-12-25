d238bcd43725cf94ea119281e87444807015e86d
Bryan Lemus <brian.lemus@outlook.com>
2023-12-24 23:58:15 -0600

    build: :construction: Firebase Config
    Initial firebase storage configuration.
    
    - Add a new directory named firebase

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDJzcf-EhsNYIfYN05XmsPzdqByqXT-zbk",
+  authDomain: "sumi-cff63.firebaseapp.com",
+  projectId: "sumi-cff63",
+  storageBucket: "sumi-cff63.appspot.com",
+  messagingSenderId: "658277686530",
+  appId: "1:658277686530:web:31aad2000c646e3a09761b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
