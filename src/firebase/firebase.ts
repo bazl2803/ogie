8ef0f3640dcbc25379a52e69eb11b529469c2447
Bryan Lemus <brian.lemus@outlook.com>
2024-01-03 16:30:34 -0600

    refactor: :sparkles: Firebase Firestore
    Firebase Firestore Config

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,7 +12,10 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { app, auth, provider }
+const db = getFirestore(app);
+
+export { app, auth, provider, db }
