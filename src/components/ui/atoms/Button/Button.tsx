ad4997836ca65f402af669460b4b457c21408cc2
Bryan Lemus <brian.lemus@outlook.com>
2024-01-30 09:14:35 -0600

    refactor: :art: Resign style tokens
    Refactor css styles and tokens and palettes.

--- src/components/ui/atoms/Button/Button.tsx
+++ src/components/ui/atoms/Button/Button.tsx
@@ -3,69 +3,70 @@
 /**
  * Dependencies
  */
-import './.Button.scss';
+import './Button.scss'
 
-import clsx from 'clsx';
-import React from 'react';
-import { Button as AriaButton, ButtonProps } from 'react-aria-components';
+import clsx from 'clsx'
+import React from 'react'
+import { Button as AriaButton, ButtonProps } from 'react-aria-components'
 
 /**
  * Props
  */
 interface IButtonProps extends ButtonProps {
-	className?: string
-	children?: React.ReactNode
-	color?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info'
-	disabled?: boolean
-	leftSide?: React.ReactNode
-	rightSide?: React.ReactNode
-	shape?: 'rounded' | 'pill'
-	size?: 'small' | 'medium' | 'large'
-	variant?: 'filled' | 'outline' | 'tonal' | 'text'
+    className?: string
+    children?: React.ReactNode
+    color?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info'
+    disabled?: boolean
+    leftSide?: React.ReactNode
+    rightSide?: React.ReactNode
+    shape?: 'rounded' | 'pill'
+    size?: 'small' | 'medium' | 'large'
+    variant?: 'filled' | 'outline' | 'tonal' | 'text'
 }
 
 /**
  * Represents a standard button element with various props for customization.
  */
-export function Button({
-	children,
-	className,
-	color,
-	shape,
-	size,
-	leftSide,
-	rightSide,
-	variant,
-	...props
-}: IButtonProps) {
-	// Classnames Variance
-	const ButtonClasses = clsx('Button', {
-		[`Button-color-${color}`]: color,
-		[`Button-shape-${shape}`]: shape,
-		[`Button-size-${size}`]: size,
-		[`Button-variant-${variant}`]: variant,
-		'Button-disabled': props.disabled,
-		[className || '']: className,
-	})
+export function Button(
+    {
+        children,
+        className,
+        color,
+        shape,
+        size,
+        leftSide,
+        rightSide,
+        variant,
+        ...props
+    }: IButtonProps) {
+    // Classnames Variance
+    const ButtonClasses = clsx('Button', {
+        [`Button-color-${color}`]: color,
+        [`Button-shape-${shape}`]: shape,
+        [`Button-size-${size}`]: size,
+        [`Button-variant-${variant}`]: variant,
+        'Button-disabled': props.disabled,
+        [className || '']: className,
+    })
 
-	// JSX
-	return (
-		<AriaButton
-			{...props}
-			className={ButtonClasses}>
-			<div className='Button-leftSide'>{leftSide}</div>
-			<div className='Button-content'>{children}</div>
-			<div className='Button-rightSide'>{rightSide}</div>
-		</AriaButton>
-	)
+    // JSX
+    return (
+        <AriaButton
+            {...props}
+            className={ButtonClasses}>
+            <div className='Button-leftSide'>{leftSide}</div>
+            <div className='Button-content'>{children}</div>
+            <div className='Button-rightSide'>{rightSide}</div>
+        </AriaButton>
+    )
 }
 
 /**
  * Default Values
  */
 Button.defaultProps = {
-	color: 'primary',
-	shape: 'rounded',
-	size: 'medium',
-	variant: 'filled',
+    color: 'primary',
+    shape: 'rounded',
+    size: 'medium',
+    variant: 'filled',
 }
