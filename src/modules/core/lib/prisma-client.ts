c897d3e1d79e40656029b37860df61b41b91ea1b
Bryan Lemus <bryanalelemus@gmail.com>
2025-04-25 18:57:04 -0600

    Modify prisma singleton

--- src/modules/core/lib/prisma-client.ts
+++ src/modules/core/lib/prisma-client.ts
@@ -1,14 +1,9 @@
-// lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const prisma = new PrismaClient();
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
